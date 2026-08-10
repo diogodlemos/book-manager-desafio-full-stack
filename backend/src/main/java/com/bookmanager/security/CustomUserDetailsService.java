2bc120957e00aebc2393db46c92dc13d3c36d344
Diogo Lemos <diogodrummond@hotmail.com>
2026-08-10 13:29:19 -0300

    configuração docker

---
+++ backend/src/main/java/com/bookmanager/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.bookmanager.security;
+
+import com.bookmanager.entities.User;
+import com.bookmanager.repositories.UserRepository;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(@NonNull String email)
+            throws UsernameNotFoundException {
+
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() ->
+                        new UsernameNotFoundException("Usuário não encontrado"));
+
+        return org.springframework.security.core.userdetails.User
+                .withUsername(user.getEmail())
+                .password(user.getPassword())
+                .authorities("USER")
+                .build();
+    }
+}
